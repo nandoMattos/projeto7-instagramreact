9e488aba7a96273c4777bfa82b6c54841f0c4993
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2022-09-16 11:27:57 -0300

    Componentização

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Corpo from "./conteudo-principal/Corpo";
+import Navbar from "./Navbar";
+
+export default function App() {
+    return(
+        <div class="container">
+            <Navbar/>
+    
+            <Corpo/>
+        </div>
+    )
+}
